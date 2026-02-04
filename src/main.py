600f15fd117565fd76f9293fea9681d0045fcd31
Markus Weinzettl <futuresystems1323@gmail.com>
2026-02-04 15:29:59 +0100

    color seetings edit

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ async def main(page: flet.Page):
     # 1. Das Hintergrundbild vorbereiten
     bg_image = flet.Image(
         src="assets/background_0.png",
-        fit="cover",
+        fit=flet.BoxFit.COVER,
         width=page.window.width,
         height=page.window.height,
     )
@@ -25,7 +25,7 @@ async def main(page: flet.Page):
     ui_layout = flet.Column(
         controls=[
             flet.Container( #Header
-                content=flet.Text("Rezept Manager 9000", color=app_colors.GHOST_WHITE, size=20, weight="bold"),
+                content=flet.Text("Rezept Manager 9000", color=app_colors.GHOST_WHITE, size=20, weight=flet.FontWeight.BOLD),
                 padding=20,
                 bgcolor=app_colors.MAGENTA,
                 alignment=flet.Alignment.CENTER,
@@ -33,7 +33,7 @@ async def main(page: flet.Page):
                 margin=10
             ),
             flet.Container( #Header_main_menu
-                content=flet.Text("Hauptmenü", size=20, weight="bold"),
+                content=flet.Text("Hauptmenü", size=20, weight=flet.FontWeight.BOLD),
                 padding=20,
                 width=300,
                 bgcolor=app_colors.ELECTRIC_SKY_BLUE,
@@ -43,12 +43,12 @@ async def main(page: flet.Page):
             ),
             flet.Row(
                 controls=[ #menu_buttons
-                    flet.Button("Alle Rezepte Anzeigen"),
-                    flet.Button("Rezept hinzufügen"),
-                    flet.Button("Rezept suchen"),
-                    flet.Button("Rezepte Namen ändern"),
-                    flet.Button("ID's anzeigen"),
-                    flet.Button("Programm beenden")
+                    flet.Button("Alle Rezepte Anzeigen",color=app_colors.GHOST_WHITE, bgcolor=app_colors.MAGENTA),
+                    flet.Button("Rezept hinzufügen",color=app_colors.GHOST_WHITE, bgcolor=app_colors.MAGENTA),
+                    flet.Button("Rezept suchen",color=app_colors.GHOST_WHITE, bgcolor=app_colors.MAGENTA),
+                    flet.Button("Rezepte Namen ändern",color=app_colors.GHOST_WHITE, bgcolor=app_colors.MAGENTA),
+                    flet.Button("ID's anzeigen",color=app_colors.GHOST_WHITE, bgcolor=app_colors.MAGENTA),
+                    flet.Button("Programm beenden",color=app_colors.GHOST_WHITE, bgcolor=app_colors.MAGENTA)
                 ],
                 wrap=True,  # Falls die Buttons zu breit für den Bildschirm sind
                 spacing=10
